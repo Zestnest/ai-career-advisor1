2e0d88e64c189addfdffeced2a4298118529fe5e
Zestnest <zestnest25@gmail.com>
2025-07-11 18:17:59 +0530

    Create BriefcaseIcon.tsx

---
+++ src/components/icons/BriefcaseIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const BriefcaseIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.07a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18.22V14.15M16.5 18.22v-6.75a2.25 2.25 0 0 0-2.25-2.25H9.75A2.25 2.25 0 0 0 7.5 11.47v6.75M16.5 7.5h-9a2.25 2.25 0 0 0-2.25 2.25v1.5H18.75v-1.5a2.25 2.25 0 0 0-2.25-2.25Z" />
+  </svg>
+);
