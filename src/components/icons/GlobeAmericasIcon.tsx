792b7f09c3e1842d68b6fffde8b52c322c37a1c4
Zestnest <zestnest25@gmail.com>
2025-07-11 18:19:14 +0530

    Create GlobeAmericasIcon.tsx

---
+++ src/components/icons/GlobeAmericasIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const GlobeAmericasIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6H21" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 3.75a14.31 14.31 0 0 1-10.5 0" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 20.25a14.31 14.31 0 0 0-10.5 0" />
+  </svg>
+);
