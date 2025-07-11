c549e6d63ea55b6ce691c8698642c538b333af0e
Zestnest <zestnest25@gmail.com>
2025-07-11 18:18:50 +0530

    Create RocketLaunchIcon.tsx

---
+++ src/components/icons/RocketLaunchIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const RocketLaunchIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.82m5.84-2.56a6 6 0 0 1-5.84 7.38v-4.82m5.84-2.56a6 6 0 0 1 5.84 2.56m-11.68 0a6 6 0 0 1 5.84-2.56m-5.84 2.56a6 6 0 0 1-5.84-2.56m11.68 0a6 6 0 0 1 2.92 4.82m-8.76 0a6 6 0 0 1 2.92 4.82m.08 0a6.002 6.002 0 0 1-5.84-7.38m5.84 7.38a6.002 6.002 0 0 0 5.84-7.38" />
+  </svg>
+);
